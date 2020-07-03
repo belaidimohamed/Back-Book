078e18927ef07efb727ba4b5bb40562f3c051010
belaidi mohamed <mohamedbelaidi408@gmail.com>
2020-07-03 12:29:54 +0100

    biz

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class Messages(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE,default=None)
     friend = models.ForeignKey(User, on_delete=models.CASCADE,default=None)
     def __str__(self):
-        return str(user)+' --> '+str(friend) +' : '+self.message
+        return str(self.user)+' --> '+str(self.friend) +' : '+self.message
 class Friends(models.Model):
     user = models.ForeignKey(UserProfile,on_delete=models.CASCADE,default=None)
     friend = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
